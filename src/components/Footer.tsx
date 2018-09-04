ab5983b1f5829d25fc6c1f70ddbaa69c361b8412
Niklas <me@sodapop.se>
2018-09-04 18:07:54 +0200

    Add build version to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,5 @@
 import * as React from "react"
 
-export const Footer: React.SFC = () => <div className="Footer">Version 20180903 &copy; Majesto Games</div>
+export const Footer: React.SFC = () => (
+  <div className="Footer">Version {process.env.REACT_APP_BUILD_VERSION || "dev"} &copy; Majesto Games</div>
+)
